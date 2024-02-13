9aa06ebac6a875582d26e9e2bd48feee5b6a10f9
Mtessier809 <82081099+Mtessier809@users.noreply.github.com>
2024-02-12 19:50:53 -0800

    created carousel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { HeaderMenu } from '../components/HeaderMenu/HeaderMenu';
-
+import { CardsCarousel } from '@/components/CardsCarousel/CardsCarousel';
+import { Stack } from '@mantine/core';
+import '@mantine/carousel/styles.css';
 export default function HomePage() {
   return (
     <>
-      <HeaderMenu/>
-      <Welcome />
-      <ColorSchemeToggle />
+      <HeaderMenu />
+      <Stack h={300} bg="var(--mantine-color-body)" gap="xl">
+        <CardsCarousel carouselTitle="Featured" />
+        <CardsCarousel carouselTitle="SAT" />
+      </Stack>
     </>
   );
 }
